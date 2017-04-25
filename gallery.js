a5bacb211db7a9753b3d744d248c4afa13365f89
Unknown <anave1@mail.csuchico.edu>
2017-04-25 15:48:42 -0700

    Random
    Selects out of my 5 favorite places in chico, without choosing the same one twice in a row.

--- gallery.js
+++ gallery.js
@@ -1,6 +1,11 @@
-
-
+var chico = ["Flumes", "Bear Hole", "CSU Chico", "Lower Bidwell", "Monkey Face"];
+var current = " ";
 function shuffleGallery()
 {
-
+var rand = Math.floor(Math.random() * (chico.length-1));
+while(current == chico[rand]){
+rand = Math.floor(Math.random() * (chico.length-1));
+}
+current = chico[rand];
+console.log(chico[rand]);
 }
