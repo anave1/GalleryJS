160369f04d16e08abb2008bb9a91c7d3b5dd7636
Unknown <anave1@mail.csuchico.edu>
2017-04-27 13:59:19 -0700

    Array of Objects

--- gallery.js
+++ gallery.js
@@ -1,4 +1,23 @@
-var chico = ["Flumes", "Bear Hole", "CSU Chico", "Lower Bidwell", "Monkey Face"];
+var chico = [
+  {description:"Flumes",
+  picture: "https://snowbrains.com/wp-content/uploads/2015/05/unnamed1.jpg",
+  from: "snowbrains.com"},
+
+  {description:"Bear Hole",
+  picture: "https://trappedinchico.files.wordpress.com/2010/05/dam2.jpg",
+  from: "trappedinchico.wordpress.com"},
+
+  {description:"CSU Chico",
+  picture: "http://www.csuchico.edu/maps/images/maps-tours-photos/mapstours-tours.jpg",
+  from: "csuchico.edu"},
+
+  {description:"Lower Bidwell",
+  picture: "http://bidwellpark.org/wp-content/uploads/2014/04/BidwellPool.jpg",
+  from: "bidwellpark.org"},
+
+  {description:"Monkey Face",
+  picture: "http://3.bp.blogspot.com/_cWoV7_rCgjk/TLVPoxIQwNI/AAAAAAAAA2s/Gpq6hGU-FX8/s1600/monkeyface2.jpg",
+  from: "blogspot.com"}];
 var current = " ";
 function shuffleGallery()
 {
